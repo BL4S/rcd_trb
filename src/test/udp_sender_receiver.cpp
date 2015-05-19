d799769f49a1e706082488ab783e3fc27ec8f069
Atlas Daqsup <daquser.cern.ch>
2015-05-19 19:10:18 +0200

    generate a random number for event length in bytes add a new menus named to send and receive TRB file

--- src/test/udp_sender_receiver.cpp
+++ src/test/udp_sender_receiver.cpp
@@ -7,16 +7,16 @@
 /*                                                                      */
 /**************** C 2015 - A nickel program worth a dime ****************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/types.h>
+#include <stdio.h>  //for printf
+#include <stdlib.h> //for exit(0)
+#include <errno.h>  //for errno - the error number
+#include <signal.h> // defines how a program handles various signals while it executes.
+#include <sys/types.h> //for data types
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <netdb.h>
+#include <sys/socket.h>  //for socket ofcourse
+#include <netinet/in.h>  //for internet doamin address (contains constans and structures)
+#include <string.h> //memset
+#include <netdb.h> //for network database operation
 #include "trb.h"
 #include "get_input.h"
 /**************/
@@ -27,14 +27,20 @@ void open_receiver(void);
 void open_sender(void);
 void receive(void);
 void send(void);
+void send_TRB(void);
+void open_TRB_File(void);
+void send_TRB_File(void);
+void receive_TRB_File(void);
+void close_TRB_File(void);
 void close_receiver(void);
 void close_sender(void);
 /***********/
 /* Globals */
 /***********/
 int sock_receive,sock_send;
-int length_send;
+int lengthOfSockaddr_send;
 struct sockaddr_in server_send;
+FILE* fd_file ;
 
 /*****************************/
 int main(int argc, char **argv)
@@ -52,6 +58,11 @@ int main(int argc, char **argv)
     printf("   10 Open sender\n");
     printf("   11 Send messages\n");
     printf("   12 Close sender\n");
+    printf("   20 Send TRB messages\n");
+    printf("   21 Open TRB File\n");
+    printf("   22 Send TRB File\n");
+    printf("   23 Receive TRB File\n");
+    printf("   24 Close TRB File\n");
     printf("   0 Exit\n");
     printf("Your choice ");
     fun = getdecd(fun);
@@ -62,6 +73,11 @@ int main(int argc, char **argv)
     if (fun == 10) open_sender();
     if (fun == 11) send();
     if (fun == 12) close_sender();
+    if (fun == 20) send_TRB();
+    if (fun == 21) open_TRB_File();
+    if (fun == 22) send_TRB_File();
+    if (fun == 23) receive_TRB_File();
+    if (fun == 24) close_TRB_File();
   }
   exit(0);
 
@@ -88,7 +104,7 @@ void open_receiver(void)
     trbport = getdecd(1234);
     printf(" Port Number = %d\n",trbport);
     
-    sock_receive=socket(AF_INET, SOCK_DGRAM, 0);
+    sock_receive=socket(AF_INET, SOCK_DGRAM, 0);//create a raw socket to receive
    if (sock_receive < 0)
   {
      perror("Failed to create socket");
@@ -138,7 +154,7 @@ void open_sender(void)
     }
     bcopy((char *)hp->h_addr, (char *)&server_send.sin_addr, hp->h_length);
     server_send.sin_port = htons(trbport);
-    length_send=sizeof(struct sockaddr_in);
+    lengthOfSockaddr_send=sizeof(struct sockaddr_in);
 }
 void receive(void)
 {
@@ -188,7 +204,7 @@ void send(void)
     printf("Going to send the message ...\n");
   //Send a message to a server from client
   for (int i=0; i<n_send; i++){
-  n = sendto(sock_send, (char *)buffer, n_length*4, 0, (struct sockaddr *)&server_send,length_send);
+  n = sendto(sock_send, (char *)buffer, n_length*4, 0, (struct sockaddr *)&server_send,lengthOfSockaddr_send);
   if (n < 0)
   {
     perror("Failed to send the data");
@@ -196,30 +212,209 @@ void send(void)
   }
   if(n_seconds >0)
   {
-    sleep(n_seconds);
+    usleep(n_seconds);
   }
  }
 }
+void send_TRB(void)
+{
+  int n_send;
+  int n_length;
+  int m_seconds;
+  int micro_seconds;
+  int n;
+  unsigned int  buffer[1024];
+
+  printf("Number of messages = ");
+  n_send=getdecd(1);
+  printf("Number of m_seconds to sleep = ");
+  m_seconds=getdecd(0);
+  micro_seconds=(m_seconds)*1000;
+
+  //Send a message to a server from client
+
+  for (int i=0; i<n_send; i++){
+
+    float ran_01=float(rand())/RAND_MAX;
+    int ran_010 = 10*ran_01;
+    printf("ran_010=%d\n" ,ran_010);
+
+    buffer[0]=(50+ran_010)*4;
+    n_length = buffer[0];
+
+    for (int j=1 ; j<n_length/4; j++)
+      {
+        buffer[j]=j+3;
+      }
+
+    printf("Going to send the message ...\n");
+
+    n = sendto(sock_send, (char *)buffer, n_length , 0, (struct sockaddr *)&server_send,lengthOfSockaddr_send);
+
+    if (n < 0)
+    {
+      perror("Failed to send the data");
+      exit(-1);
+    }
+    if(m_seconds >0)
+    {
+      usleep(micro_seconds);
+    }
+  }
+}
+void open_TRB_File(void)
+{
+  unsigned int file_buf[256];
+
+  fd_file = fopen("network.bin","r");
+
+  if ( fd_file < 0)
+    {
+      perror(" failed to open file");
+      exit(-1);
+    }
+}
+void send_TRB_File(void)
+{
+  //unsigned int trb_header[3];
+  unsigned int trb_data[256];
+  int n_words;
+
+      printf("fd_file = %d\n",fd_file);
+
+      n_words = fread(trb_data,sizeof(unsigned int),3,fd_file); // read three integers to our buffer
+      printf("n_words : when reading header = %d\n",n_words);
+      if ( n_words < 0) 
+        {
+          perror(" failed to read file");
+          exit(-1);
+        }
+      for (int i = 0; i<n_words; i++)
+        {
+          printf(" trb_data[%d] = %x \n",i,trb_data[i]);
+        }
+
+      if (trb_data[1]!=0x30063)
+        {
+          printf("trb_data[1] = %x\n");
+          exit(-1);
+        }
+
+      int n_length = trb_data[2]/sizeof(unsigned int) - 3;
+      n_words = fread(&trb_data[3],sizeof(unsigned int),
+      n_length,
+      fd_file); // read event data
+
+      if ( n_words < n_length)
+        {
+          printf(" failed to read file\n");
+          printf("n_words : %d\n",n_words);
+          printf("n_length :%d\n",n_length);
+          exit(-1);
+        }
+
+      for (int i = 3; i<(n_words+3); i++)
+        {
+          printf(" trb_data[%d] = %x\n",i,trb_data[i]);
+        }
+
+      printf("Going to send the message ...\n");
+
+      int n = sendto(sock_send, (char *)trb_data, trb_data[2] , 0, (struct sockaddr *)&server_send,lengthOfSockaddr_send);
+
+      if (n < 0)
+        {
+          perror("Failed to send the data");
+          exit(-1);
+        }
+}
+void receive_TRB_File(void)
+{
+  int trb_recive;
+  unsigned int  trb_data[256];
+  int n_bytes;
+  socklen_t fromlen;
+  struct sockaddr_in from;
+  fromlen = sizeof(struct sockaddr_in);
+  
+  printf ("Number of messages =");
+  trb_recive=getdecd(1);
+  for (int i=0; i<trb_recive; i++)
+  {
+  ////Receive  messages on the server
+    printf("Waiting for message...\n");
+
+    n_bytes = recvfrom(sock_receive,trb_data,3*sizeof(unsigned int),
+                     MSG_PEEK, (struct sockaddr *)&from,&fromlen);
+  
+    if (n_bytes < 0)
+      {
+        perror("Failed to receive the data");
+        exit(-1);
+      }
+    printf("Received a datagram with length %d\n",n_bytes);
+  
+    for(int i=0; i<n_bytes/sizeof(unsigned int) ; i++)
+      {
+      
+        printf("word number %d = %d/%x\n", i, trb_data[i], trb_data[i]);
+      }
+
+    int n_max_bytes = trb_data[2];
+    printf("Max bytes = %d\n" , n_max_bytes);
+    
+    n_bytes = recvfrom(sock_receive,trb_data, n_max_bytes, 
+                      0, (struct sockaddr *)&from,&fromlen);
+  
+    if (n_bytes < 0)
+      {
+        perror("Failed to receive the data");
+        exit(-1);
+      }
+    
+    printf("Received a datagram with length %d\n",n_bytes);
+  
+    for(int i=0; i<n_bytes/sizeof(unsigned int) ; i++)
+      {
+        printf("word number %d = %d/%x\n", i, trb_data[i], trb_data[i]);
+      }
+  }
+}
+void close_TRB_File(void)
+{
+  unsigned int file_buf[256];
+  fclose(fd_file);
+
+  if ( fd_file < 0)
+    {
+      perror(" failed to close file");
+      exit(-1);
+    }
+}
+
 //close the socket
+
 void close_receiver(void)
 {
   int  ret;
   ret = close(sock_receive);
-   printf(" return = %d, errno = %d\n",ret,errno);
+  printf(" return = %d, errno = %d\n",ret,errno);
 
-   if (ret <0)
+  if (ret <0)
    {
-    perror("close_error");
+      perror("close_error");
    }
 }
+
 void close_sender(void)
 {
   int ret;
   ret = close(sock_send);
-   printf(" return = %d, errno = %d\n",ret,errno);
-   if (ret <0)
-   {
-    perror("close_error");
-   }
+  printf(" return = %d, errno = %d\n",ret,errno);
+  
+  if (ret <0)
+    {
+      perror("close_error");
+    }
 }
 
